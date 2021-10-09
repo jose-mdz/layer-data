c42d230c2a5c2210d6379def74bdbeec458a50dd
Jose Menendez <josemenendez@autonomic.ai>
2021-10-08 18:24:21 -0700

    upgrader

--- lib/sqlite/SQLite.d.ts
+++ lib/sqlite/SQLite.d.ts
@@ -1,10 +1,11 @@
-import { Database, RunResult, Statement } from "sqlite3";
+import { Database, RunResult } from "sqlite3";
 import { DataColumnType, DataSource, DataTableDefinition, EntityMapper } from "../DataSource";
 import { SchemaOf } from "layer-validation";
 import { SQLiteSqlGenerator } from "./SQLiteSqlGenerator";
 export interface SQLiteConfig {
     callback?: () => void;
     echoSQL?: boolean;
+    echoErrorSQL?: boolean;
 }
 export declare class SQLite implements DataSource {
     readonly path: string;
@@ -14,13 +15,15 @@ export declare class SQLite implements DataSource {
     readonly sqlGenerator: SQLiteSqlGenerator;
     constructor(path: string, config?: SQLiteConfig);
     private sqlOut;
+    private sqlOutError;
     getClosestType(type: DataColumnType): string;
     insert<T>(data: T, schema: SchemaOf<T>, mapper: EntityMapper<T>): Promise<T>;
     update<T>(data: T, schema: SchemaOf<T>, mapper: EntityMapper<T>): Promise<void>;
     delete<T>(data: T, schema: SchemaOf<T>, mapper: EntityMapper<T>): Promise<void>;
     createTable(table: DataTableDefinition): Promise<any>;
     getTableNames(): Promise<string[]>;
-    queriesRun(sql: string): Promise<Statement>;
+    queriesRun(sql: string): Promise<void>;
+    private run;
     queryRun(query: string, params?: object | any[]): Promise<RunResult>;
     queryData(query: string, params?: object | any[]): Promise<any[]>;
     queryEntity<T>(schema: SchemaOf<T>, query: string, params?: object | any[]): Promise<T[]>;
